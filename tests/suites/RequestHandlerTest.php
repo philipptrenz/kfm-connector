f2a9c54e1e85aa080c11f2765882b93bc16e8e67
Philipp Trenz <mail@philipptrenz.de>
2023-09-26 20:39:33 +0200

    fix JWKS cache refetch, add rate limiting for JWKS cache refetch

--- tests/suites/RequestHandlerTest.php
+++ tests/suites/RequestHandlerTest.php
@@ -497,6 +497,121 @@ final class RequestHandlerTest extends TestCase {
         );
     }
 
-    
+    public function testRateLimitExceeded() : void
+    {
+        $cert = new JwtCertificate();
+
+        // Populate cache with jwks
+        $this->cache->set(RequestHandler::$JWKS_CACHE_KEY, JwtCertificate::toJWKS($cert));
+
+        $cert2 = new JwtCertificate();
+        $jwt = $cert2->issueJWT($this->issuer, $this->audience);
+
+        $this->kirby = new App([
+			'server' => [
+				'HTTP_AUTHORIZATION' => 'Bearer ' . $jwt
+            ],
+		]);
+
+        $h = new RequestHandler($this->cache, 'http://localhost:9000/', $this->issuer, $this->cacheDuration);
+        try {
+            $h->isAuthorized(new Request, $this->kirby->visitor());
+            $this->assertTrue(false);
+        } catch(JwksException $e) {
+            // Expect connection error
+            $this->assertTrue(true);
+        }
+
+        // Check if rate limit cache value is set correctly
+        $this->assertEquals(1, $this->cache->get(RequestHandler::$RATE_LIMIT_CACHE_KEY));
+        $initalExpiresAt = $this->cache->expires(RequestHandler::$RATE_LIMIT_CACHE_KEY);
+        $this->assertGreaterThanOrEqual(time() + 60, $initalExpiresAt);
+
+        for($i=2; $i<=10; $i++) {
+
+            // Re-populate cache with jwks
+            $this->cache->set(RequestHandler::$JWKS_CACHE_KEY, JwtCertificate::toJWKS($cert));
+
+            try {
+                $h->isAuthorized(new Request, $this->kirby->visitor());
+                $this->assertTrue(false);
+            } catch(JwksException $e) {
+                // Expect connection error
+                $this->assertTrue(true);
+            }
+
+            // Check if rate limit cache value is set correctly
+            $this->assertEquals($i, $this->cache->get(RequestHandler::$RATE_LIMIT_CACHE_KEY));
+            $expiresAt = $this->cache->expires(RequestHandler::$RATE_LIMIT_CACHE_KEY);
+            $this->assertGreaterThanOrEqual(time() + 60, $expiresAt);
+            $this->assertEquals($initalExpiresAt, $expiresAt);
+        }
+
+        // Re-populate cache with jwks
+        $this->cache->set(RequestHandler::$JWKS_CACHE_KEY, JwtCertificate::toJWKS($cert));
+
+        // Assert, that isAuthorized() returns false without doing JWKS lookup
+        $this->assertFalse(
+            $h->isAuthorized(new Request, $this->kirby->visitor())
+        );
+
+    }
+
+    public function testRateLimitCacheCounterReset() : void
+    {
+        $cert = new JwtCertificate();
+
+        // Populate cache with jwks
+        $this->cache->set(RequestHandler::$JWKS_CACHE_KEY, JwtCertificate::toJWKS($cert));
+
+        $cert2 = new JwtCertificate();
+        $jwt = $cert2->issueJWT($this->issuer, $this->audience);
+
+        $this->kirby = new App([
+			'server' => [
+				'HTTP_AUTHORIZATION' => 'Bearer ' . $jwt
+            ],
+		]);
+
+        $h = new RequestHandler($this->cache, 'http://localhost:9000/', $this->issuer, $this->cacheDuration);
+        try {
+            $h->isAuthorized(new Request, $this->kirby->visitor());
+            $this->assertTrue(false);
+        } catch(JwksException $e) {
+            // Expect connection error
+            $this->assertTrue(true);
+        }
+
+        // Check if rate limit cache value is set correctly
+        $this->assertEquals(1, $this->cache->get(RequestHandler::$RATE_LIMIT_CACHE_KEY));
+        $initalExpiresAt = $this->cache->expires(RequestHandler::$RATE_LIMIT_CACHE_KEY);
+        $this->assertGreaterThanOrEqual(time() + 60, $initalExpiresAt);
+
+        for($i=2; $i<=7; $i++) {
+
+            sleep(10);
+
+            // Re-populate cache with jwks
+            $this->cache->set(RequestHandler::$JWKS_CACHE_KEY, JwtCertificate::toJWKS($cert));
+
+            try {
+                $h->isAuthorized(new Request, $this->kirby->visitor());
+                $this->assertTrue(false);
+            } catch(JwksException $e) {
+                // Expect connection error
+                $this->assertTrue(true);
+            }
+
+            $expiresAt = $this->cache->expires(RequestHandler::$RATE_LIMIT_CACHE_KEY);
+            if ($initalExpiresAt > time()) {    // within first minote
+                $this->assertEquals($i, $this->cache->get(RequestHandler::$RATE_LIMIT_CACHE_KEY));
+                $this->assertEquals($initalExpiresAt, $expiresAt);
+            } else {                            // after one minute
+                $this->assertLessThan(6, $this->cache->get(RequestHandler::$RATE_LIMIT_CACHE_KEY));
+                $this->assertGreaterThanOrEqual($initalExpiresAt + 60, $expiresAt);
+            }
+        }
+
+    }
 
 }
